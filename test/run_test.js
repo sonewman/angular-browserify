7ba33c3c14361acbc7cb763872ad70b19411c221
Sam Newman <acutecoder@gmail.com>
2013-10-13 03:10:17 +0100

    updated dependencies moved "thru" and "bashy" to their own module

--- test/run_test.js
+++ test/run_test.js
@@ -1,10 +1,10 @@
 #!/usr/bin/env node
 /* jshint laxcomma:true */
-var spawnify = require('../lib/spawnify')
-  , browserifyTest = spawnify('./node_modules/.bin/browserify ./test/test.js -o ./test/test-bundle.js');
+var bashy = require('bashy')
+  , browserifyTest = bashy('./node_modules/.bin/browserify ./test/test.js -o ./test/test-bundle.js');
 
 browserifyTest.pipe(process.stdout);
 browserifyTest.on('end', function () {
-  spawnify('./node_modules/.bin/phantomjs ./test/test.js')
+  bashy('./node_modules/.bin/phantomjs ./test/test.js')
     .pipe(process.stdout);
 });

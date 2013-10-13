209fbadc52a9ac98c6b0f3683a417227bc455136
Sam Newman <acutecoder@gmail.com>
2013-10-13 20:47:51 +0100

    fixed build reference to module which has been moved and renamed

--- build.js
+++ build.js
@@ -2,7 +2,5 @@
 /* jshint laxcomma:true */
 
 // TODO add windows check
-var spawnify = require('./lib/spawnify');
-
-spawnify('cd ./node_modules/angularjs && grunt package')
+require('bashy')('cd ./node_modules/angularjs && grunt package')
   .pipe(process.stdout);
